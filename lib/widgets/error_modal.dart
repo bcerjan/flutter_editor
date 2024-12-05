e445433212bee930f613a960b3b7b03a32a1a76d
Ben Cerjan <bcerjan@gmail.com>
2024-12-04 20:38:34 -0500

    Updated so dismiss button actually dismissed the modal

--- lib/widgets/error_modal.dart
+++ lib/widgets/error_modal.dart
@@ -1,16 +1,21 @@
 import 'package:editor/services/ui/modal.dart';
+import 'package:editor/services/ui/ui.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ErrorModal extends StatelessWidget {
   const ErrorModal({super.key, required this.text});
   final String text;
-  static const List<UIButton> btns = [
-    UIButton(
-      text: 'Dismiss',
-    ),
-  ];
+
   @override
   Widget build(BuildContext context) {
+    final ui = Provider.of<UIProvider>(context, listen: false);
+    final List<UIButton> btns = [
+      UIButton(
+        text: 'Dismiss',
+        onTap: () => ui.clearPopups(),
+      ),
+    ];
     return UIModal(
       title: 'Error: ',
       message: text,
